0c67d0be8bfc115102be026b912beebb3b0156ac
Anup Kishore <anup@secondspectrum.com>
2017-05-03 10:19:42 -0700

    First pass attempt at p12. Broken

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+numFactors :: Int -> Int
+numFactors x =
+  let top = sqrt x
+  in [i | i <- [1 .. floor (top)], i < top, x `mod` i == 0]
+
+p12 :: Int -> Int
+p12 x = head . filter (> 500) . map numFilters . scanl1 (+) $ [1 ..]
