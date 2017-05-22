4d4c50ccfae12c16e98606804891280afdc3bbab
Anup Kishore <anup@secondspectrum.com>
2017-05-22 02:55:23 -0700

    Working 14

--- src/Problem14.hs
+++ src/Problem14.hs
@@ -1,3 +1,17 @@
-module Problem14 (p14) where
+module Problem14
+  ( p14
+  ) where
 
-p14 = ""
+import Data.List (maximum, maximumBy)
+import Data.Ord (comparing)
+
+collatz' :: Int -> Int -> Int
+collatz' i n
+  | n == 1 = i
+  | n `mod` 2 == 0 = collatz' (i + 1) $ n `div` 2
+  | otherwise = collatz' (i + 1) $ (3 * n) + 1
+
+collatz :: Int -> Int
+collatz = collatz' 0
+
+p14 = show . maximumBy (comparing collatz) $ [1 .. 1000000]
