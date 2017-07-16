0fd95d9ce16288edd034d9d171182336515995c0
Anup Kishore <anup@secondspectrum.com>
2017-07-16 00:39:18 -0700

    Spec p15

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
 import Problem12 (p12)
 import Problem13 (p13)
 import Problem14 (p14)
+import Problem15 (p15)
 
 problem :: String -> Maybe String
-problem = flip lookup [("12", p12), ("13", p13), ("14", p14)]
+problem = flip lookup [("12", p12), ("13", p13), ("14", p14), ("15", p15)]
---
+++ src/Problem15.hs
@@ -0,0 +1,4 @@
+module Problem15 where
+
+p15 :: String
+p15 = "None"
